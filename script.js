d2ee2b009d2bd4eaf4a8bb1b52005bfa5836502d
Van Nguyen <vannguyen11a21@gmail.com>
2025-08-11 12:35:16 +0700

    set effect for button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Lấy ra elements của trang 
-
 const form = document.getElementById("form");
 const inputEmailElement = document.getElementById("inputEmail");
 const inputPasswordElement = document.getElementById("inputPassword");
@@ -7,6 +6,18 @@ const inputPasswordElement = document.getElementById("inputPassword");
 // Lấy ra elements error của trang
 const errorEmailElement = document.getElementById("emailError") 
 const errorPasswordElement = document.getElementById("passwordError");
+
+// Lấy ra button GG và GitHub
+const buttonGGElement = document.getElementById("button-Google");
+const buttonGitHubElement = document.getElementById("button-GitHub");
+
+// Lấy URl của Sign In gg 
+const urlGGSignIn = "https://accounts.google.com/o/oauth2/v2/auth" +
+        "?client_id=YOUR_CLIENT_ID.apps.googleusercontent.com" +
+        "&redirect_uri=YOUR_REDIRECT_URI" +
+        "&response_type=code" +
+        "&scope=email%20profile%20openid" +
+        "&access_type=online";
 
 // lắng nghe sự kiện submit của form
 form.addEventListener("submit", function(event){
@@ -27,3 +38,12 @@ form.addEventListener("submit", function(event){
     }
 })
 
+// Lắng nghe sự kiện click của button GG
+buttonGGElement.addEventListener("click", function(){
+   window.open(urlGGSignIn, "_blank");
+})
+
+// Lắng nghe sự kiện click của button GitHub
+buttonGitHubElement.addEventListener("click", function(){
+    window.open("https://github.com/login", "_blank");
+})
